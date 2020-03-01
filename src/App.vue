02b5d7c09e9c349dd0cab31ebd851fe9d4bf8c0b
Ayman <aelba103@uottawa.ca>
2020-02-29 21:19:00 -0500

    trying to write or read something in db :/

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,68 @@
 <template>
   <div id="app">
+    <div>
+      <input type="text"
+        v-model="newReptile"
+        @keyup.enter="addReptile">
+      <button  @click="addReptile">
+        Add 
+      </button>
+    </div>
+    <ul class="reptileList">
+      <li v-for="reptile in reptiles" >
+        {{ reptile.name }} -
+        <button @click="deleteReptile(reptile)">
+          Remove
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import { db } from './firebase';
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        reptiles: [],
+        newReptile: ''
+      }
+    },
+    firestore() {
+      return {
+        reptiles: db.collection('reptiles'),
+      }
+    },
+    methods: {
+      addReptile: function() {
+        this.$firestore.reptiles.add(
+          {
+            name: this.newReptile,
+            timestamp: new Date()
+          }
+        );
+        this.newReptile = '';
+      },
+      deleteReptile: function(reptile) {
+        this.$firestore.reptiles.doc(reptile['.key']).delete();
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .reptileList {
+    list-style: none;
+  }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
+import VueFirestore from 'vue-firestore';
+
+// turns off the 'You are running Vue in development mode.' msg
+Vue.config.productionTip = false;
+
+Vue.use(VueFirestore);
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  template: '<App/>',
+  components: { App },
 })
