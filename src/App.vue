b411f7730d7a5e3905a4f216c50eec4d3bb78430
Ayman <aelba103@uottawa.ca>
2020-02-29 23:23:21 -0500

    user can now create a recipe

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,68 @@
 <template>
   <div id="app">
     <div>
-      <input type="text"
+      <input type="textarea"
         v-model="newtitle"
         placeholder="The title of the recipe"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newprepTime"
         placeholder="Preparation Time"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newcookTime"
         placeholder="Cook Time"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newreadyTime"
         placeholder="Ready Time (estimative)"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient1"
         placeholder="Ingredient 1"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient2"
         placeholder="Ingredient 2"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient2"
         placeholder="Ingredient 2"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient3"
         placeholder="Ingredient 3"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient4"
         placeholder="Ingredient 4"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient5"
         placeholder="Ingredient 5"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient6"
         placeholder="Ingredient 6"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient7"
         placeholder="Ingredient 7"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient8"
         placeholder="Ingredient 8"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient9"
         placeholder="Ingredient 9"
         @keyup.enter="addarecipe">
-        <input type="text"
+        <input type="textarea"
         v-model="newingredient10"
         placeholder="Ingredient 10"
         @keyup.enter="addarecipe">
-      <button  @click="addarecipe">
+        <br>
+        <button  @click="addarecipe">
         Add a recipe
       </button>
     </div>
@@ -146,9 +147,6 @@ methods: {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  }
-  .reptileList {
-    list-style: none;
   }
 </style>
 
