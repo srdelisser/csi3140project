9f023c585f555aa0403305c87c198a8fdd114e1b
Ayman <aelba103@uottawa.ca>
2020-02-29 23:07:58 -0500

    add addrecipepage

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,69 @@
   <div id="app">
     <div>
       <input type="text"
-        v-model="newReptile"
-        @keyup.enter="addReptile">
-      <button  @click="addReptile">
-        Add 
+        v-model="newtitle"
+        placeholder="The title of the recipe"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newprepTime"
+        placeholder="Preparation Time"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newcookTime"
+        placeholder="Cook Time"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newreadyTime"
+        placeholder="Ready Time (estimative)"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient1"
+        placeholder="Ingredient 1"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient2"
+        placeholder="Ingredient 2"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient2"
+        placeholder="Ingredient 2"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient3"
+        placeholder="Ingredient 3"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient4"
+        placeholder="Ingredient 4"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient5"
+        placeholder="Ingredient 5"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient6"
+        placeholder="Ingredient 6"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient7"
+        placeholder="Ingredient 7"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient8"
+        placeholder="Ingredient 8"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient9"
+        placeholder="Ingredient 9"
+        @keyup.enter="addarecipe">
+        <input type="text"
+        v-model="newingredient10"
+        placeholder="Ingredient 10"
+        @keyup.enter="addarecipe">
+      <button  @click="addarecipe">
+        Add a recipe
       </button>
     </div>
-    <ul class="reptileList">
-      <li v-for="reptile in reptiles" >
-        {{ reptile.name }} -
-        <button @click="deleteReptile(reptile)">
-          Remove
-        </button>
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -26,28 +75,66 @@
     name: 'app',
     data() {
       return {
-        reptiles: [],
-        newReptile: ''
+        recipes: [],
+        newtitle: '',
+        newcookTime: '',
+        newreadyTime: '',
+        newprepTime: '',
+        ingredient1: '',
+        ingredient2: '',
+        ingredient3: '',
+        ingredient4: '',
+        ingredient5: '',
+        ingredient6: '',
+        ingredient7: '',
+        ingredient8: '',
+        ingredient9: '',
+        ingredient10: ''
       }
     },
     firestore() {
       return {
-        reptiles: db.collection('reptiles'),
+        recipes: db.collection('recipes'),
       }
     },
-    methods: {
-      addReptile: function() {
-        this.$firestore.reptiles.add(
+methods: {
+      addarecipe: function() {
+        this.$firestore.recipes.add(
           {
-            name: this.newReptile,
+            title: this.newtitle,
+            prepTime: this.newprepTime,
+            cookTime: this.newcookTime,
+            readyTime: this.newreadyTime,
+            ingredient1: this.newingredient1,
+            ingredient2: this.newingredient2,
+            ingredient3: this.newingredient3,
+            ingredient4: this.newingredient4,
+            ingredient5: this.newingredient5,
+            ingredient6: this.newingredient6,
+            ingredient7: this.newingredient7,
+            ingredient8: this.newingredient8,
+            ingredient9: this.newingredient9,
+            ingredient10: this.newingredient10,
             timestamp: new Date()
           }
         );
-        this.newReptile = '';
+        recipes = [],
+        newtitle = '',
+        newcookTime = '',
+        newreadyTime = '',
+        newprepTime = '',
+        newingredient1 = '',
+        newingredient2 = '',
+        newingredient3 =  '',
+        newingredient4 =  '',
+        newingredient5 =  '',
+        newingredient6 = '',
+        newingredient7 = '',
+        newingredient8 = '',
+        newingredient9 = '',
+        newingredient10 =  ''
+        
       },
-      deleteReptile: function(reptile) {
-        this.$firestore.reptiles.doc(reptile['.key']).delete();
-      }
     }
   }
 </script>
