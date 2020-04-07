632c61b72142d5d9620ddf60a85c824b62cb9ce0
Ayman <aelba103@uottawa.ca>
2020-04-07 16:20:58 -0400

    added vue-tabs to main.js in order to use it in account view

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App';
 import Register from './Register';
 import VueFirestore from 'vue-firestore';
+import {Tabs, Tab} from 'vue-tabs-component';
 
 // turns off the 'You are running Vue in development mode.' msg
 Vue.config.productionTip = false;
@@ -20,3 +21,6 @@ new Vue({
   template: '<Register/>',
   components: { Register },
 })
+
+Vue.component('tabs', Tabs);
+Vue.component('tab', Tab);
